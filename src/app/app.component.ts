3d65b590be8ce6ac5af8256756b01a6df4f71ac6
Quentin Laplanche <quentin.laplanche@outlook.fr>
2016-11-20 20:22:57 +0100

    Added Basic grid and person

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MdSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isDarkTheme: boolean = false;
   title = 'app works!';
+
+  persons = [
+    {rows: 2, name: 'Quentin', role: 'Capitaine'},
+    {rows: 1, name: 'Jeremy', role: 'Front'},
+    {rows: 1, name: 'Constantin', role: 'Unknown'},
+    {rows: 2, name: 'Guillaume', role: 'Unknown'},
+    {rows: 1, name: 'Meryem', role: 'Unknown'},
+    {rows: 2, name: 'Louis', role: 'Unknown'},
+    {rows: 1, name: 'Thierry-Philippe', role: 'Unknown'},
+    {rows: 2, name: 'David', role: 'Cafe'},
+    {rows: 2, name: 'Camille', role: 'Unknown'},
+    {rows: 1, name: 'Than', role: 'Unknown'}
+  ];
+  @ViewChild('sidenav') sidenav: MdSidenav;
+  currentPerson = {};
+
+  showPerson(person: any) {
+    this.currentPerson = person;
+    this.sidenav.open();
+  }
 }

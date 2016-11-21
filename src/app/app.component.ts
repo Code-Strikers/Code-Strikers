b1bad11aab07ee90ce4bbc0c59b1b14d3a6bdeaa
Jeterim <Jeterim@users.noreply.github.com>
2016-11-22 00:04:08 +0100

    Correction caractères 'utf'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,13 @@ export class AppComponent {
   persons = [
     {rows: 2, name: 'Quentin', role: 'Capitaine',
       citation:  'Il n\'y a que deux sortes de langages de programmation: ceux dont les gens disent toujours du mal et ceux que personne n\'utilise.'},
-    {rows: 1, name: 'Jérémy', role: '(Front/Back?)end Developer', citation: 'Don’t worry if it doesn’t work right. If everything did, you’d be out of a job.'},
+    {rows: 1, name: 'Jérémy', role: '(Front/Back?)end Developer', citation: 'Don\'t worry if it doesn\'t work right. If everything did, you\'d be out of a job.'},
     {rows: 1, name: 'Constantin', role: 'Relations Internationales', citation: ''},
     {rows: 2, name: 'Guillaume', role: 'Backend Developer', citation: ''},
     {rows: 1, name: 'Louis', role: 'Frontend Developer', citation: ''},
     {rows: 2, name: 'Meryem', role: 'Frontend Developer', citation: ''},
     {rows: 1, name: 'Thierry-Philippe', role: 'Backend Developer', citation: ''},
-    {rows: 2, name: 'David', role: 'Cafe - Frontend Developer à temps partiel', citation: ''},
+    {rows: 2, name: 'David', role: 'Café - Frontend Developer à temps partiel', citation: ''},
     {rows: 2, name: 'Camille', role: 'Backend Developer', citation: ''},
     {rows: 1, name: 'Than', role: 'Unknown', citation: ''}
   ];
